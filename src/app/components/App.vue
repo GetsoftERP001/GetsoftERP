1cff17e874d7c4c7ddf26ad865ae37a41a8f6181
GetsoftERP0001 <jhonny.chamba@getsoft.com.ec>
2022-04-07 09:09:40 -0500

    Primer Commit Jc

---
+++ src/app/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <a href="/" class="navbar-brand">Menu</a>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" placeholder="Inserta un texto" class="form-control">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
+</template>
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const { use } = require('./routes/tareas');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/tareas', require('./routes/tareas'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), () => {
+  console.log('Se esta ejecutando en el puerto => ' + app.get('port'));
+});
