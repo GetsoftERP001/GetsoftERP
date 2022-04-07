ee1fdf3b93dae4d88078a01422df752c10c22ada
GetsoftERP0001 <jhonny.chamba@getsoft.com.ec>
2022-04-07 10:09:55 -0500

    Quinto Commit modificado App.vue Jc

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,44 +1,28 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-dark bg-primary">
       <div class="container-fluid">
-        <a href="/" class="navbar-brand">Menu</a>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul
-                class="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+        <a href="/" class="navbar-brand">Header</a>
       </div>
     </nav>
+    <div class="container">
+      <div class="row pt-5">
+        <div class="col-md-5">
+          <div class="card">
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    placeholder="Inserta un texto"
+                    class="form-control"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
